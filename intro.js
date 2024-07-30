25f3eb59b729dfa4db4caeb6a6d367a896ab2b37
aashess <aashesh.j@gmail.com>
2024-07-31 00:31:16 +0530

    typeof datatype

--- intro.js
+++ intro.js
@@ -1,1 +1,19 @@
-console.log("Aashish")
+/* var name = "Aashish"
+
+let student = "20"
+
+teacher = "Om"
+
+console.log(student)
+
+console.table([name,student,teacher])
+
+name = "hello"
+teacher = "Prem"
+student = "00"
+console.table([name,student,teacher])*/
+
+let student = "aashish"
+console.log(typeof student)
+console.log(student)
+
