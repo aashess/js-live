d0d238e350701126435d00692414fa76e75a6f86
aashess <aashesh.j@gmail.com>
2024-10-31 02:57:39 +0530

    operations

--- index.js
+++ index.js
@@ -6,5 +6,18 @@ let score = ""
 
 let value = Boolean(score)
 
-console.log(typeof value)
-console.log(value)
+// console.log(typeof value)
+// console.log(value)
+// console.log(2+2)
+// console.log(2**6)
+
+// let str1 = "aashish"
+// let str2 = " jha"
+// str3 = str1 + str2
+// console.log(str3)
+
+console.log("2"+2)
+console.log("2"+"2")
+console.log("2"+2+2)
+console.log(2+2+"2")
+
