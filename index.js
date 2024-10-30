1379d1c1dcafba5a26470bd4132611b2606e849d
aashess <aashesh.j@gmail.com>
2024-10-31 02:21:54 +0530

    Conversion of Datatype

---
+++ index.js
@@ -0,0 +1,10 @@
+//console.log(typeof(score))
+//console.log("hello world.")
+//console.log(typeof score)
+
+let score = ""
+
+let value = Boolean(score)
+
+console.log(typeof value)
+console.log(value)
