d079a33ac3980bf813ba1a614a31d0b135603df3
aashess <aashesh.j@gmail.com>
2024-11-01 21:55:29 +0530

    stack&heap

--- index.js
+++ index.js
@@ -16,8 +16,64 @@ let value = Boolean(score)
 // str3 = str1 + str2
 // console.log(str3)
 
-console.log("2"+2)
-console.log("2"+"2")
-console.log("2"+2+2)
-console.log(2+2+"2")
+// console.log("2"+2)
+// console.log("2"+"2")
+// console.log("2"+2+2)
+// console.log(2+2+"2")
+
+//////////Comparison of Datatypes//////////
+
+// console.log("03">2);
+// console.log("3">2);
+
+//here javascript converts string to a number. 
+//JavaScript converts strings to numbers during comparisons to make the operation more 
+//logical and meaningful.
+
+//////////////////////////////////////RESEACH//////////////
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null === 0);
+
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined > 0)
+
+////////////////////////////////////////////////////////////
+
+const sc = 20
+const st = 100.20
+
+let aashish //no need to intalized. and can be changed later as well.
+
+const aash =20  //is constant can't be changed it later. and need to intalized at the time of 
+                        //declaration. 
+
+
+const aashs = null
+
+///symbol is created. //unique id. 
+
+const sym = Symbol("123")
+const symboll = Symbol("123")
+
+// console.log(sym === symboll);  
+
+const bigNumber = 163747564737n //big int
+
+///////////////////////////
+//arrays//
+
+// heros = ["superman","captain america","Ironman"]     //arrays 
+
+// heroos = {                  //objects
+//     name : "ironman",
+//     age : 20,
+//     journal : "straight"
+// }
+// ******************* End *********************
+
+
 
---
+++ stack&heap.js
@@ -0,0 +1,10 @@
+// let name = "hitesh"
+
+// let another = name
+
+// another = "chaudhary" 
+
+// console.log(name);
+// console.log(another);
+
+
