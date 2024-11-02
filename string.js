800bad33d2fe9ac732541ab605c722eeb9cd0fa0
aashess <aashesh.j@gmail.com>
2024-11-02 20:31:40 +0530

    stringInDetail

---
+++ string.js
@@ -0,0 +1,25 @@
+let name = "aashish"
+let age = 20;
+
+//this is how you define string. 
+
+//console.log(`This is another ${name} and his age: ${age}`);
+
+// console.log("This is how " + name + " could be done. ");
+
+
+const varr = "aashish"
+
+console.log(`This is another form of making money.${varr}`);
+
+// you can define string my this method also... this is called manually defining. 
+
+const str = new String("aashish")
+console.log(str);
+
+const num = new Number(10)
+console.log(num);
+
+
+const str1 = new String('aash')
+console.log(str1);
