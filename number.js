bf4ce3d6837d6ebd2b642f6a890945912ee08e6e
aashess <aashesh.j@gmail.com>
2024-11-08 08:54:36 +0530

    number function

---
+++ number.js
@@ -0,0 +1,33 @@
+//function toTitleCase(str) {
+    //     let number1 = str.split(" ")
+    
+    //     for (let index = 0; index < number1.length; index++) {
+    //    //const element = array[index];
+    //         number1[index] = number1[index].charAt(0).toUpperCase() + number1[index].slice(1).toLowerCase()
+    
+    
+    //     }
+    
+    //     return number1.join(" ")
+        
+    // }
+    
+    // console.log(toTitleCase("aashish kUmAr JhA is soMeHow TO BE ABLE TO Make"));
+
+
+    //tried to use split(). 
+
+// let name = "aashish is somehow"
+// let brea = name.split(" ")
+
+// console.log(brea[0]);
+
+// console.log(brea.join(" "));
+// console.log(brea.slice(1));
+
+// //tried to use slice and uppercase to see is it working or not. 
+// let alphabet = "AASHISH"
+
+// let capital_alphabet =alphabet.slice(2).toLowerCase()
+// console.log(capital_alphabet);
+
