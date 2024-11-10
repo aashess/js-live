301eaade216125f37bf05baecbc6c2719e29d5d8
aashess <aashesh.j@gmail.com>
2024-11-10 13:59:20 +0530

    dates function

--- dates.js
+++ dates.js
@@ -1,5 +1,40 @@
-let my_date = new Date(2023,2,17)
+// let my_date = Date()
 
-console.log(my_date);
-console.log(Date());
+// console.log(my_date);
+// console.log(Date());
 
+// Array Methods. 
+
+// const myarr = new Array(2,4,6,7)
+// console.log(myarr);
+
+// myarr.push(4)
+// myarr.push(22)
+// console.log(myarr);
+// myarr.pop()
+// console.log(myarr);
+// console.log(myarr.includes(4));
+// console.log(myarr.indexOf(4))
+// console.log(myarr.length);
+
+// let arr1 = (2,34,5,5,77)
+// let arrSlice = arr1.slice(1,4)
+// console.log(arrSlice)
+// let arrsplice = arr1.splice(1,4)
+// console.log(arrsplice)
+
+// let arr2 = (3,5,7,86,5,4,3)
+
+let date = new Date()
+
+console.log(date);
+console.log(date.toString());
+console.log(date.toISOString());
+console.log(date.getFullYear());
+console.log(date.toDateString());
+console.log(date.toJSON());
+
+console.log(typeof date)
+
+let date1 = new Date(2024,2,31)
+console.log(date1.toDateString())
