004b032a0239bcd701cf7bb1221c9a27937f3c9b
aashess <aashesh.j@gmail.com>
2024-11-11 01:42:43 +0530

    array function

---
+++ array.js
@@ -0,0 +1,39 @@
+//Array Function************************* 
+
+// let name1 = [2,35,3,2,3,4,5]
+
+// name1.push(99)
+// console.log(name1);
+
+// name1.pop()
+// console.log(name1);
+
+//Shift and Unshift Function************ 
+
+// name1.shift()
+// console.log(name1);
+
+// const arr = name1.join()
+// console.log(typeof arr);
+
+//Slice and Splice********************
+// let arr1 = [2,3,4,6]
+// arr_0 = arr1.slice(1,3)
+// console.log("A ", arr_0);
+// arr_00 = arr1.splice(1,3)
+// console.log("B ", arr_00);
+
+//Concat and Spread Function 
+
+const arr1 = ["aashish","bikash","bibek","om"]
+const arr2 = ["jha","yadav", "mishra", "jha"]
+
+// console.log("concat: ",arr1.concat(arr2));
+
+// console.log("push: ", arr1.push(arr2));
+
+arr_0 = [...arr1, ...arr2]
+console.log("Spread: ", arr_0);
+
+
+
