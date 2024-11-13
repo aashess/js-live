bdf49ee99cc5088b764dbb8f374aa21681bc8c79
aashess <aashesh.j@gmail.com>
2024-11-13 05:56:11 +0530

    object

---
+++ object1.js
@@ -0,0 +1,37 @@
+const obj1 = {
+    name : "aashish jha",
+    id : 26345,
+    gender : "male",
+    color: "Dark",
+
+}
+
+// console.log(obj1["gender"]);
+// console.log(obj1.name);
+// console.log(obj1.gender);
+
+// console.log(obj1["name"]);
+
+
+obj1.greeting = function() {
+    welcome_greet : "Welcome to AirBnb in Noida."
+}
+
+// console.log(obj1.greeting());
+
+// console.log(obj1.name);
+// console.log("******************");
+// console.log(obj1);
+
+const object_1 = {
+    id : 3404,
+    full_name : {
+        first_name : "aashish",
+        last_name : " jha"
+    },
+    address : "Kanpur, Uttarpardesh"
+}
+
+console.log(object_1.full_name.first_name," is first person to come here.");
+
+
