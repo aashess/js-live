dcf51fdedda427f5dd0f3aded1b626757e138107
aashess <aashesh.j@gmail.com>
2024-11-14 08:10:45 +0530

    youtube_question

--- question/qs_3.js
+++ question/qs_3.js
@@ -10,12 +10,20 @@ function countVowels(data) {
     const arr_word = lower_letter.split("")
 
     console.log(arr_word);
+    new_array_index = 0
+    count = 0 
+    const index2 = 0
 
-    vowels.forEach(single_vowels => {
-        if (    ) {
-            
+
+    for (let index = 0; index <= arr_word.length; index++) {
+        if (arr_word[index2] === vowels[index]) {
+            count++ 
+            index2++
         }
-    });
+            
+        
+    }
+
     
-}
+
 const numofVowels = countVowels(str);
