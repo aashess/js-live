bdfbacc25896c3aa7fb295e8bd52aa3d6512bf43
aashess <aashesh.j@gmail.com>
2024-11-25 22:59:44 +0530

    scope & hostining

--- scope.js
+++ scope.js
@@ -1,18 +1,60 @@
-var a = 20
+// // var a = 20
 
-let b = 21
+// // let b = 21
 
-if (true) {
-    console.log("inner function: ", a);
-    console.log("inner function: ", a);
+// // if (true) {
+// //     console.log("inner function: ", a);
+// //     console.log("inner function: ", a);
 
-    a = 30
-    b = 31
+// //     a = 30
+// //     b = 31
     
-    console.log(a);
-    console.log(b);
+// //     console.log(a);
+// //     console.log(b);
     
+// // }
+
+// // console.log(a);
+// // console.log(b);
+
+// function one(){
+//     const username = "hitesh"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log("website");
+
+//      two()
+
+// }
+
+// one()
+
+// sccopeeeeeeeeee
+
+// if (true) {
+//     const username = "hitesh"
+//     if (username == "hitesh"){
+//         const var1 = "website"
+
+//         console.log(username  + var1+ " inner inner inner");
+        
+//     }
+//     console.log(username  + " inner inner");
+//     return username
+// }
+ 
+// console.log("inner format.");
+
+console.log(func1(5));
+
+function func1(num) {
+    return num + 1
 }
 
-console.log(a);
-console.log(b);
+console.log(func_2());
+
+
+
